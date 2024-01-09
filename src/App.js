687078cf17532d7ea8cc892b276fc8d0315eb306
AHMEDAGNAOU <72224528+AHMEDAGNAOU@users.noreply.github.com>
2024-01-09 17:30:04 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, themeProvider } from "@mui/material";
+import Topbar from "./scenes/global/Topbar";
 
 function App() {
+  const [theme, colorMode] = useMode();
+
   return (
-    <div className="app">
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <themeProvider theme={theme}>
+        <CssBaseline/>
+        <div className="app">
+          <main className="content">
+            <Topbar/>
+          </main>
+        </div>
+      </themeProvider>
+    </ColorModeContext.Provider>
+    
   );
 }
 
